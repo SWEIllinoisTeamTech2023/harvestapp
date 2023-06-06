a92237ded4c3caccb0389cf0bc7f3c11cd254d5e
kriti-mathur1 <thekritimathur@gmail.com>
2023-06-05 22:06:47 -0500

    start nav bar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,8 +5,34 @@ import { useState } from "react";
 function Navbar() {
   const [isOpen, setOpen] = useState(false);
   return (
-    <div className="NavBar">
-      <Hamburger toggled={isOpen} toggle={setOpen} />
+    <div className="NavBar" id="NavBar1">
+      <Hamburger
+        onToggle={(toggled) => {
+          if (toggled) {
+            document.getElementById("NavMenu").style.visibility = "visible";
+            console.log("open");
+            // openMenu();
+          } else {
+            document.getElementById("NavMenu").style.visibility = "hidden";
+            console.log("closed");
+            // closeMenu();
+          }
+        }}
+      />
+      <section className="NavMenu" id="NavMenu"></section>
+    </div>
+  );
+}
+
+function openMenu() {
+  console.log("hi");
+}
+
+function closeMenu() {
+  console.log("bye");
+  return (
+    <div>
+      <h1>close</h1>
     </div>
   );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Header from "../components/header";
+import "../styles/home.css";
+import Navbar from "../components/navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="Home">
       <Header title="Home"></Header>
       {/* <Link to="/signup">
         <button>Create a new account</button>
