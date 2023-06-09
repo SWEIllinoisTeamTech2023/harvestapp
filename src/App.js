1e02d896165c75e1ae9d011c54dc94e18889205f
kriti-mathur1 <thekritimathur@gmail.com>
2023-06-09 18:33:27 -0500

    hopefully fixed aws-amplify installing error

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import InputData from "./pages/inputData";
 import Simulate from "./pages/simulate";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
 
 function App() {
+  Amplify.configure(awsExports);
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route exact path="/" element={<CreateAccount />} /> */}
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<CreateAccount />} />
+          {/* <Route exact path="/" element={<Home />} /> */}
           <Route exact path="/login" element={<Login />} />
           {/* <Route exact path="/signup" element={<CreateAccount />} /> */}
           <Route exact path="/inputdata" element={<InputData />} />
