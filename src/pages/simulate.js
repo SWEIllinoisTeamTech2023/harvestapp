fdb2d94023813b5ab8a62151c5cfcd38f04d5652
kriti-mathur1 <thekritimathur@gmail.com>
2023-07-13 00:16:45 -0500

    add input parameters

--- src/pages/simulate.js
+++ src/pages/simulate.js
@@ -3,6 +3,7 @@ import { Auth } from "aws-amplify";
 import { Link } from "react-router-dom";
 import Header from "../components/header";
 import "../styles/simulate.css";
+import { TextField } from "@mui/material";
 
 const Simulate = () => {
   const [user, setUser] = useState();
@@ -28,7 +29,75 @@ const Simulate = () => {
       <div class="simulate-parent">
         <div class="edit-simulation">
           <h2>Input Parameters</h2>
+          <div class="input-variables"></div>
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Chaffer Clearance"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Concave Clearance"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Sieve Clearance"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Harvest Cost"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Speed"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Fan Speed"
+          />
+          <input
+            className="inputBox"
+            type="number"
+            // value={yieldValue}
+            // onChange={handleYieldChange}
+            min="0"
+            placeholder="Rotor Speed"
+          />
+          <button
+            className="button"
+            type="submit"
+            style={{ marginTop: "50px" }}
+          >
+            Save Edited Variables
+          </button>
+          <button className="button" type="submit">
+            Copy Optimized Variables
+          </button>
         </div>
+
         <div class="display-simulation"></div>
       </div>
     </div>
