b2e4a37ff6b56b28b8e096f9f26eeff85c1a3e4c
kriti-mathur1 <thekritimathur@gmail.com>
2023-07-16 15:12:43 -0500

    linegraph being a butt

--- src/pages/simulate.js
+++ src/pages/simulate.js
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import "../styles/simulate.css";
 import { TextField } from "@mui/material";
 import Piechart from "../components/piechart";
+import Linegraph from "../components/linegraph";
 
 const Simulate = () => {
   const [user, setUser] = useState();
@@ -27,154 +28,86 @@ const Simulate = () => {
   return (
     <div>
       <Header title="Simulate"></Header>
-      {/* <div class="simulate-parent">
-        <div class="edit-simulation">
-          <h2>Input Parameters</h2>
-          <div class="input-variables"></div>
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Chaffer Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Concave Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Sieve Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Harvest Cost"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Speed"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Fan Speed"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Rotor Speed"
-          />
-          <button
-            className="button"
-            type="submit"
-            style={{ marginTop: "50px" }}
-          >
-            Save Edited Variables
-          </button>
-          <button className="button" type="submit">
-            Copy Optimized Variables
-          </button>
+      <div class="simulate-parent">
+        <div class="simulate-piechart">
+          <div class="input-variables">
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Chaffer Clearance"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Concave Clearance"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Sieve Clearance"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Harvest Cost"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Speed"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Fan Speed"
+            />
+            <input
+              className="inputBox"
+              type="number"
+              // value={yieldValue}
+              // onChange={handleYieldChange}
+              min="0"
+              placeholder="Rotor Speed"
+            />
+            <button
+              className="button"
+              type="submit"
+              style={{ marginTop: "30px" }}
+            >
+              Save Edited Variables
+            </button>
+            <button className="button" type="submit">
+              Copy Optimized Variables
+            </button>
+          </div>
+          <div class="display-simulation">
+            <h2>Cost Distribution</h2>
+            <Piechart />
+          </div>
         </div>
-        <div class="display-simulation">
-          <h2>Pie Chart</h2>
-          <Piechart />
-        </div>
-      </div> */}
-      <div class="simulate-piechart">
-        <div class="input-variables">
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Chaffer Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Concave Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Sieve Clearance"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Harvest Cost"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Speed"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Fan Speed"
-          />
-          <input
-            className="inputBox"
-            type="number"
-            // value={yieldValue}
-            // onChange={handleYieldChange}
-            min="0"
-            placeholder="Rotor Speed"
-          />
-          <button
-            className="button"
-            type="submit"
-            style={{ marginTop: "30px" }}
-          >
-            Save Edited Variables
-          </button>
-          <button className="button" type="submit">
-            Copy Optimized Variables
-          </button>
-        </div>
-        <div class="display-simulation">
-          <h2>Pie Chart</h2>
-          <Piechart />
+        <div class="simulate-linegraph">
+          <div class="display-simulation">
+            <h2>Cost vs Feedrate</h2>
+            <Linegraph />
+          </div>
         </div>
       </div>
     </div>
